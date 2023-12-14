bb10d9dfc6387bd0ea19921bd18525c113aad995
Daniel Lindgren <daniel.lindgren.88.so@gmail.com>
2023-12-14 11:53:17 +0100

    Final change. Compete program

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,11 +11,6 @@ public class Main {
     }
 }
 
-//Welcome to the adventure of Farm Simulator!
-//In this program you can make different actions from the menu such as:
-//Look at all crops and their quantity, Add new crops with a specified amount. You can also remove an unnecessary crop from the farm.
-//Look at all animals and what they are eating. Add new animals with a specific fed type to make the animal as happy as can be! You can also remove any animals from the farm.
-//If you have any future question or suggestions please report.
 
 
 
